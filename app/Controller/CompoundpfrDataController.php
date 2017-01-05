931bd8e8b72869fb8651ae3b25b74809e4562347
tonymcghie <tonymcghienz@gmail.com>
2017-01-05 21:55:11 +1300

    added Andrew's changes to fix the page length and the match/graph problem

--- app/Controller/CompoundpfrDataController.php
+++ app/Controller/CompoundpfrDataController.php
@@ -49,9 +49,10 @@ class CompoundpfrDataController extends AppController{
         if (!isset($this->request->data['Compoundpfr_data'])){
             return;
         }//if no data is passed return and dont search
-        $this->paginate = array(
-        'limit' => 40,
-        'order' => array('Compoundpfr_data.assigned_name' => 'asc'));           
+        //$this->paginate = array(
+        //'limit' => 40,
+        //'order' => array('Compoundpfr_data.assigned_name' => 'asc')); 
+        $this->Paginator->settings = array('limit' => 100);
         $this->request->data['Compoundpfr_data']['num_boxes'] = (isset($this->request->data['Compoundpfr_data']['num_boxes']) ? $this->request->data['Compoundpfr_data']['num_boxes'] : 1); //sets boxnum to 1 if its not already set
         $this->set('box_nums',$this->request->data['Compoundpfr_data']['num_boxes']); //passes the number of boxes from the old form to the new form
         if ($this->request->data['Compoundpfr_data']['isDate']==='1'){
@@ -66,8 +67,8 @@ class CompoundpfrDataController extends AppController{
         //$search = $this->My->extractSearchTerm($this->request->data, ['assigned_name', 'assigned_confid', 'exact_mass', 'intensity_description', 'reference', 'sample_ref', 'crop', 'species', 'tissue', 'genotype', 'analyst', 'file'], 'Compoundpfr_data');
         $search = $this->addPsu($this->request->data, $search); //adds the synonims to the search array
 //        print_r($search);
-        echo $this->paginate['limit'];
-        var_dump($search);
+//        echo $this->paginate['limit'];
+//        var_dump($search);
         $this->set('results' ,$this->paginate('Compoundpfr_data', $search)); //sets the results from the cake pagination helper
 //        var_dump($this->paginate('Compoundpfr_data', $search));
         $this->set('num', $this->Compoundpfr_data->find('count', ['conditions' =>$search]));
@@ -115,11 +116,12 @@ class CompoundpfrDataController extends AppController{
             $data['Compoundpfr_data']['cri_'.$count] = $pair['cri'];
             $data['Compoundpfr_data']['val_'.$count] = $pair['val'];
             $data['Compoundpfr_data']['log_'.$count] = $pair['log'];
+            $data['Compoundpfr_data']['match_'.$count] = $pair['match'];
             $count++;
         } //adds the search criteia value and logic to an array in the same structure as if it came from a form
         $options = $this->request->data['options']; // contains the options such as the pivot
         //gets the criteria for the search
-        $search = $this->My->extractSearchTermGraph($data, ['assigned_name', 'assigned_confid', 'exact_mass', 'intensity_description', 'reference', 'sample_ref', 'crop', 'species', 'tissue', 'genotype', 'analyst'], 'Compoundpfr_data');               
+        $search = $this->My->extractSearchTerm($data, ['assigned_name', 'assigned_confid', 'exact_mass', 'intensity_description', 'reference', 'sample_ref', 'crop', 'species', 'tissue', 'genotype', 'analyst'], 'Compoundpfr_data');               
         $search = $this->addPsu($data, $search); //adds psydonims from compund table to the search in OR array
         $data = $this->Compoundpfr_data->find('all', ['conditions' => $search]); //finds the data 
         //return;
