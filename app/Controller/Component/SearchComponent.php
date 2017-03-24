26e5ffa7d46bb527ab6c60fa034aa9d05192e9e9
mgoo <andrewmcghienz@gmail.com>
2017-03-24 23:12:39 +1300

    skips sets with empty search

--- app/Controller/Component/SearchComponent.php
+++ app/Controller/Component/SearchComponent.php
@@ -9,9 +9,10 @@
 App::uses('Component', 'Controller');
 
 class SearchComponent extends Component{
+    public $excluded_fields = ['id'];
 
     /**
-     * @param $model string the name of the modle to search in
+     * @param $model AppModel the name of the modle to search in
      * @param $criteria array the array of the criteria from the search form
      * @param $value array the array of the values from the search form
      * @param $logic array the array of the logic values from the search form
@@ -25,7 +26,7 @@ class SearchComponent extends Component{
             $value_value = $value[$index];
             $logic_value = $logic[$index];
             $match_value = $match[$index];
-
+            if (empty($value_value))continue;
             switch ($match_value) {
                 case 'contains':
                     $value_value = '%'.$value_value.'%';
@@ -40,7 +41,16 @@ class SearchComponent extends Component{
                     throw new Exception('There was a match value that was not found if you are modifying the code please add it to this statment');
                     break;
             }
-            $query[$logic_value][] = [$model . '.' . $criteria_value . ' LIKE' => $value_value];
+            if ($criteria_value == 'all') {
+                foreach (array_keys($model->schema()) as $column_name) {
+                    if (in_array($column_name, $this->excluded_fields)){
+                        continue;
+                    }
+                    $query[$logic_value][] = [$model->name . '.' . $column_name . ' LIKE' => $value_value];
+                }
+            } else {
+                $query[$logic_value][] = [$model->name . '.' . $criteria_value . ' LIKE' => $value_value];
+            }
         }
         return $query;
     }
