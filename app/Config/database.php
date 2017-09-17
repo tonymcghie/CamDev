d8a791d21c0a9b3941894b92d1195715c1cee87f
mgoo <andrewmcghienz@gmail.com>
2017-09-17 13:13:28 +1200

    Revert "clean"
    This reverts commit 1afe360f82159efa4634155fda42226cf18ffbba.

---
+++ app/Config/database.php
@@ -0,0 +1,32 @@
+<?php
+class DATABASE_CONFIG {
+        /**
+         * @LIVE swap database connection
+         */
+        //local config
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false ,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'abc123',
+		'database' => 'camdata',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	);
+        //live config
+        /**public $default = [
+		'datasource' => 'Database/Mysql',
+		'persistent' => false ,
+		'host' => 'database.powerplant.pfr.co.nz',
+		'login' => 'camadmin',
+		'password' => '',
+		'database' => 'cam_data',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	];*/
+        
+        function __construct() {
+            //$this->default['password'] = getenv('MYSQL_PASS');
+        }
+}
