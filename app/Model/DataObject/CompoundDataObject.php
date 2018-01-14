c516f0cd6ec6806a8bd6249fb287572ac52dfec5
Tony McGhie <tonymcghienz@gmail.com>
2018-01-14 22:33:48 +1300

    changed the Compounds->View action to open a new tab in PubChem

--- app/Model/DataObject/CompoundDataObject.php
+++ app/Model/DataObject/CompoundDataObject.php
@@ -32,7 +32,7 @@ class CompoundDataObject extends DataObject {
         return [
             'pubchemLink' => ['https://pubchem.ncbi.nlm.nih.gov/compound/'.$this->pub_chem],
             'chemspiderLink' => ['http://www.chemspider.com/Chemical-Structure.'.$this->chemspider_id.'.html'],
-            'viewURL' => ['controller' => 'Compounds', 'action' => 'addCompound', '?' => ['id' => $this->id]],
+            'viewURL' => ['https://pubchem.ncbi.nlm.nih.gov/compound/'.$this->pub_chem],
             'editURL' => ['controller' => 'Compounds', 'action' => 'editCompound', '?' => ['id' => $this->id]]
         ];
     }
